5fde378264009aa8a0457f4d71e07fad0bda3f89
Rob Blanco <roberto.blanco@inria.fr>
2015-05-26 17:08:01 +0200

    Toggle debug mode script
    A Perl script to toggle on and off debugging, defined as those
    lines of code in .thm files annotated with %DEBUG at the end. It
    will treat all lines independently so it is sensitive to partial
    states.
    
    It wouldn't hurt to be conservative with the file names given, or
    with the execution environment. We're rewriting files, reversibly,
    but nonetheless.

---
+++ debug.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $path = shift; # Optional, should end with / if given
+my $bedwyr = $path ? $path . "bedwyr" : "bedwyr";
+
+my @files = `ls kernel/*.thm fpc/*.thm`;
+foreach my $filename (@files) {
+	chomp $filename;
+	# Slurp file
+	open FILE, $filename or die $?;
+	my @lines = <FILE>;
+	close FILE;
+	# Rewrite
+	open FILE, ">$filename" or die $?;
+	foreach my $line (@lines) {
+		if ($line =~ /^%.*%DEBUG$/) {
+			$line = substr $line, 1;
+		}
+		elsif ($line =~ /^[^%].*%DEBUG$/) {
+			$line = "%$line";
+		}
+		print FILE $line;
+	}
+	close FILE;
+}
